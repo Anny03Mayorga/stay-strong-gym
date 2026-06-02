7ad395eb3289e436c615e2e80b06d37a6d05980f
Anny Mayorga <annymayorga.ibm@latam.com>
2026-06-02 16:45:46 -0500

    fix pwa icons

--- vite.config.js
+++ vite.config.js
@@ -17,17 +17,17 @@ export default defineConfig({
         display: "standalone",
         start_url: "/",
         icons: [
-          {
-            src: "/logo2.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/logo2.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
+  {
+    src: "/logo1.png",
+    sizes: "192x192",
+    type: "image/png"
+  },
+  {
+    src: "/logo2.png",
+    sizes: "512x512",
+    type: "image/png"
+  }
+]
       }
     })
   ]
