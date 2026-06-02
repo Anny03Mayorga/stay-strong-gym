9a24fc93b812032016154044469958776276d5bb
Anny Mayorga <annymayorga.ibm@latam.com>
2026-06-01 23:58:22 -0500

    add pwa support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "logo.png", "logo1.png", "logo2.png"],
+      manifest: {
+        name: "Stay Strong Gym",
+        short_name: "Stay Strong",
+        description: "App de rutinas y seguimiento de entrenamiento",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/logo2.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/logo2.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
+});
